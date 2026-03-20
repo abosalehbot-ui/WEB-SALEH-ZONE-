e32b8ac31f0644a67f05ae3e1236ab3b2bf1a46f
ABO SALEH <abosalehbot@gmail.com>
2026-03-20 04:01:48 +0200

    Delete frontend directory

--- frontend/src/lib/theme/themes.ts
+++
@@ -1,5 +0,0 @@
-export type ThemeName = "default" | "synthwave" | "sith" | "vip";
-
-export const THEMES: ThemeName[] = ["default", "synthwave", "sith", "vip"];
-
-export const isThemeName = (value: string): value is ThemeName => THEMES.includes(value as ThemeName);
