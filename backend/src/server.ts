857c95dd3dbfd2f8882f0d87d27efd0293f91122
ABO SALEH <abosalehbot@gmail.com>
2026-03-20 04:01:30 +0200

    Delete backend directory

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const port = Number(process.env.PORT) || 8080;
-const host = "0.0.0.0";
-
-app.listen(port, host, () => {
-  console.log(`Saleh Zone backend listening on http://${host}:${port}`);
-});
